59cc052623b0f07473c766eaa3c5210e2faf16d5
simao234430 <simao234430@gmail.com>
2023-11-27 11:35:13 +0800

    deledt unuseful staff

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,10 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import {NextUIProvider} from '@nextui-org/system'
+ 
 export default function App({ Component, pageProps }: AppProps) {
  
   return  (
-  <NextUIProvider>
+ 
   <Component {...pageProps} />
-</NextUIProvider>)
+ )
 }
